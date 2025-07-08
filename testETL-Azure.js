daa8a6fcba2ed1a3b2d2f1532e64007d575d3399
Ko <sitthidetkumrungsee@gmail.com>
2025-07-08 18:19:57 +0700

    update: add seperator

--- testETL-Azure.js
+++ testETL-Azure.js
@@ -6,15 +6,21 @@ import { DocxLoader } from "@langchain/community/document_loaders/fs/docx";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
 // 🔹 โหลดไฟล์
-const loader = new CSVLoader("./docs/scores.csv");  // เปลี่ยนเป็น loader อื่นได้
-// const loader = new PDFLoader("./docs/test.pdf");  // เปลี่ยนเป็น loader อื่นได้
+// const loader = new CSVLoader("./docs/scores.csv");  // เปลี่ยนเป็น loader อื่นได้
+const loader = new PDFLoader("./docs/test.pdf");  // เปลี่ยนเป็น loader อื่นได้
 // const loader = new DocxLoader("./docs/test.docx");  // เปลี่ยนเป็น loader อื่นได้
 const docs = await loader.load();  // docs = [{ pageContent: "...", metadata: {...} }]
 
 // 🔹 ทำ Chunk
 const splitter = new RecursiveCharacterTextSplitter({
   chunkSize: 1000,
-  chunkOverlap: 100
+  chunkOverlap: 200,
+  separators: [
+    "\n\n",
+    "\n",
+    " ",
+    ""
+  ]
 });
 const chunkedDocs = await splitter.splitDocuments(docs);
 console.log("🚀 ~ chunkedDocs:", chunkedDocs)
